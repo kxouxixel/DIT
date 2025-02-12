436cff827cf859fe0d04a999e55d4f2caa8ca3b9
kxouxixel <makarenkokirill25@gmail.com>
2025-02-12 17:57:01 +0300

    Переделано

--- password.py
+++ password.py
@@ -1,8 +1,16 @@
 def ask_password(password):
     count = 0
-    while password != password:
-        if password != password:
-            count += 1
-            print('Неверный пароль! Осталось несколько попыток')
+    while True:
+        password1 = input('Введите пароль: ')
+        if count != 3:
+            if password1 != password:
+                count += 1
+                print('Неверный пароль! Осталось несколько попыток')
+        if count != 3 and password1 == password:
+            print('Пароль верный!')
+            break
         if count == 3:
             print('Попытки закончились!')
+            break
+        
+ask_password('ezkk')
